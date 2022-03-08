ffe3a784162d1ddda508928858325e6c5de49ff8
Bedirhan Dağ <bedirhan.dag@tsoft.com.tr>
2022-03-08 11:00:41 +0300

    Added SplashActivity.kt to project. Icons updated.

---
+++ app/src/main/java/com/bedirhandag/harcapaylas/ui/activity/splash/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.bedirhandag.harcapaylas.ui.activity.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bedirhandag.harcapaylas.databinding.ActivitySplashBinding
+import com.bedirhandag.harcapaylas.ui.activity.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViewBinding()
+        showSplash()
+
+    }
+
+    private fun showSplash(){
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(1500)
+                    navigateLoginActivity()
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+
+    private fun navigateLoginActivity(){
+        val intent = Intent(baseContext, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun initViewBinding(){
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
